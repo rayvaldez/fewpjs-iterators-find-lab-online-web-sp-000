2616089b2d6e606fa8a80681102b5f64635b8ca3
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-10-19 14:56:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(gameArray) {
+  let val = gameArray.find(gameObj => gameObj.result === "W");
+  if (val !== undefined) {
+    return val.year
+  } else {
+    return undefined
+  }
+}
