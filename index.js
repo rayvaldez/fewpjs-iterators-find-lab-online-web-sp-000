e376f8d0b9f9e1b55c8afab673d62b94abe8353c
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-10-19 14:55:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function testFunc() {
   return "hi"
 }
 
-function superbowlWin() {
-  
+function superbowlWin(gameArray) {
+  let val = gameArray.find(gameObj => gameObj.result === "W");
+  if (val !== undefined) {
+    return val.year
+  } else {
+    return undefined
+  }
 }
